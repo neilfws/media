c98cf36a074dbb72f5750ea30bb3bd93d7d3da01
Neil Saunders <neilfws@gmail.com>
2017-01-11 21:39:17 +1100

    Changed CSS selector, refined gsubs, remove blank words

--- ausnews/code/R/shiny/ausnewsClouds/app.R
+++ ausnews/code/R/shiny/ausnewsClouds/app.R
@@ -14,20 +14,21 @@ library(rvest)
 library(stringr)
 library(tm)
 
-fairfax_to_headlines <- function(base_url) {
-  headlines <- read_html(base_url) %>% html_nodes("div.story__wof") %>% html_text()
-  headlines <- str_match(headlines, "^\\n\\s+\\n\\s+(.*?)\\n")[, 2]
+source_to_headlines <- function(base_url) {
+  headlines <- read_html(base_url) %>% html_nodes("h3.story__headline") %>% html_text()
   # now lots of ugly munging to get to bare words
-  headlines <- gsub("\\.$", "",headlines)
-  headlines <- gsub(",", "",headlines)
-  headlines <- gsub("\\?", "",headlines)
-  headlines <- gsub("\\:", "",headlines)
-  headlines <- gsub("'\\s+", " ",headlines)
-  headlines <- gsub("\\s+'", " ",headlines)
-  headlines <- gsub("^'", "",headlines)
-  headlines <- gsub("'$", "",headlines)
-  headlines <- gsub("^\"", "",headlines)
-  headlines <- gsub("$\"", "",headlines)
+  headlines <- gsub("\\n+", "", headlines)
+  headlines <- gsub("\\.$", "", headlines)
+  headlines <- gsub(",", "", headlines)
+  headlines <- gsub("\\?", "", headlines)
+  headlines <- gsub("\\:", "", headlines)
+  headlines <- gsub("'\\s+", " ", headlines)
+  headlines <- gsub("\\s+'", " ", headlines)
+  headlines <- gsub("\\s+", " ", headlines)
+  headlines <- gsub("^'", "", headlines)
+  headlines <- gsub("'$", "", headlines)
+  headlines <- gsub("^\"", "", headlines)
+  headlines <- gsub("$\"", "", headlines)
   headlines <- tolower(headlines)
   return(headlines)
 }
@@ -35,6 +36,7 @@ fairfax_to_headlines <- function(base_url) {
 headlines_to_words <- function(headlines) {
   words <- unlist(strsplit(headlines, "\\s+"))
   words <- words[!words %in% stopwords()]
+  words <- words[-which(words == "")]
   return(words)
 }
 
@@ -81,7 +83,7 @@ ui <- fluidPage(
 server <- function(input, output) {
   
   observe({
-    headlines <- fairfax_to_headlines(input$publication)
+    headlines <- source_to_headlines(input$publication)
     headlines.words <- headlines_to_words(unique(headlines))
     words.wc <- count_words(headlines.words)
     pal2 <- brewer.pal(8, "Dark2")
